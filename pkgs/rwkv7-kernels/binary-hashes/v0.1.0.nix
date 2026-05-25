3105fd699c223322fc8fe1c0c5d5a0efa68856e5
sirati <git@sirati.me>
2026-05-25 07:21:30 +0200

    rwkv7-kernels: rebuild with torch.ops.load_library import fix

--- pkgs/rwkv7-kernels/binary-hashes/v0.1.0.nix
+++ pkgs/rwkv7-kernels/binary-hashes/v0.1.0.nix
@@ -8,7 +8,7 @@
           x86_64 = {
             name = "rwkv7_kernels-0.1.0-cp311-cp311-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl";
             url  = "https://github.com/sirati/rwkv7-wrapper/releases/download/v0.1/rwkv7_kernels-0.1.0-cp311-cp311-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl";
-            hash = "sha256-NY0WbxNGlzvdTtL/l1D3SYgCVCyyHZcVmvLBFJi2EDE=";
+            hash = "sha256-m/XUFygf5GjC5NDP8Odqu4DmBGvrhwcRlqIaIECU9kQ=";
           };
         };
       };
@@ -17,7 +17,7 @@
           x86_64 = {
             name = "rwkv7_kernels-0.1.0-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl";
             url  = "https://github.com/sirati/rwkv7-wrapper/releases/download/v0.1/rwkv7_kernels-0.1.0-cp313-cp313-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl";
-            hash = "sha256-NeZkZWhxUJjqaEZmVueqfgZlA2xIDVT1uD+dTqPIMk4=";
+            hash = "sha256-ieMRuawGPvDzPK4SBD3EHOFaA3HYkoj8JHBltCXr6og=";
           };
         };
       };
